5f590734d1fbcb188a4fe002f84d090a3aa57356
Victor Saravia <mavicdevtech@gmail.com>
2024-06-30 23:50:50 -0500

    feat: start project

---
+++ ms-generate-qr/src/main/java/com/mavicdevtech/ms_generate_qr/MsGenerateQrApplication.java
@@ -0,0 +1,13 @@
+package com.mavicdevtech.ms_generate_qr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGenerateQrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGenerateQrApplication.class, args);
+	}
+
+}
---
+++ ms-generate-qr/src/test/java/com/mavicdevtech/ms_generate_qr/MsGenerateQrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavicdevtech.ms_generate_qr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGenerateQrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
